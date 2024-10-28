b9a1f78267c65e82c7a8ec6482010e180e64bf7c
Anna Piaskovska <piaskovska.pn@ucu.edu.ua>
2024-10-28 21:34:47 +0200

    fix errors

--- src/test/java/ucu/edu/ua/apps/lab8/Lab8ApplicationTests.java
+++ src/test/java/ucu/edu/ua/apps/lab8/Lab8ApplicationTests.java
@@ -1,13 +1,12 @@
 package ucu.edu.ua.apps.lab8;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import ucu.edu.ua.apps.lab8.flower.Lab8Application;
+import org.junit.jupiter.api.Test;
 
-@SpringBootTest
-class Lab8ApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+@SpringBootTest(classes = Lab8Application.class)
+public class Lab8ApplicationTests {
+    @Test
+    void contextLoads() {
+    }
 }
